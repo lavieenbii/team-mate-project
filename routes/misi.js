d567f08b5bbffd8e95c2ab5429f1dbe3473eaa7e
[Ipung Sonia] <[ipung.sonia@mail.ugm.ac.id]>
2021-11-26 11:22:06 +0700

    first commit

--- routes/misi.js
+++ routes/misi.js
@@ -1,44 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path')
-var db = require('../db/db')
+var path = require("path");
+var db = require("../db/db");
 
-router.post('/', function(req, res, next) {
-  db.run('INSERT INTO MissionPlan(planName, g3wp) VALUES (?, ?)', 
-        [req.body.namaMisi, req.body.geoJSON],
-        (err) => {
-          if (err) {
-            console.log(err)
-            res.status(500).json({status: 'error'})
-            return
-          };
-          res.status(200).json({status: 'OK'})
-        }) 
+//load mission
+router.get("/", function (req, res, next) {
+  db.all("SELECT * FROM MissionPlan", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ status: "error" });
+      return;
+    }
+    res.json(rows);
+  });
 });
 
-router.get('/', function(req, res, next){
-    db.all('SELECT * FROM MissionPlan',
-    (err, rows) => {
-        if (err) {
-            console.log(err)
-            res.status(500).json({status: 'error'})
-            return
-        };
-        res.json(rows)
-    })
-})
-
-router.get('/delete/:id', (req, res, next) => {
-    db.run(`DELETE FROM MissionPlan WHERE planId = ?`,
-    [req.params.id],
+//create mission
+router.post("/", function (req, res, next) {
+  db.run(
+    `INSERT INTO MissionPlan(planName, g3wp) VALUES (?, ?)`,
+    [req.body.namaMisi, req.body.geoJSON],
     (err) => {
       if (err) {
-        console.log(err)
-        res.status(500).json({status: 'error'})
-        return
-      };  
-    res.status(200).json({status: 'deleted'})
-    })
-})
+        console.log(err);
+        res.status(500).send({ prompt: "error" });
+        return;
+      }
+      res.status(200).send("created");
+    }
+  );
+});
+
+//delete a mission
+router.get("/delete/:id", function (req, res) {
+  let id = req.params.id;
+  db.run(`DELETE FROM MissionPlan WHERE planId = ${id}`, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: "error",
+      });
+    }
+    res.status(200).json({ status: "deleted" });
+  });
+});
+
+//edit a mission
 
 module.exports = router;
